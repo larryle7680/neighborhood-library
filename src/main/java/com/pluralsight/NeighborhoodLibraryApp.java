45f6cd3d4fd1abef743fd80546186b12c09b4aeb
Larry <larryle7680@gmail.com>
2026-04-19 11:16:34 -0500

    Set global variables and array of 20 in NeighborhoodLibraryApp.

--- src/main/java/com/pluralsight/NeighborhoodLibraryApp.java
+++ src/main/java/com/pluralsight/NeighborhoodLibraryApp.java
@@ -1,8 +1,25 @@
 package com.pluralsight;
 
 public class NeighborhoodLibraryApp {
+    //Creating global variables, so I can access them in methods
+    static int id = 0;
+    static String isbn = "";
+    static String title = "";
+    static boolean isCheckedOut = false;
+    static String checkOutTo = "";
 
+    //Creating a global Array with an inventory of 20 books
+    static Book book[] = new Book[20];
+
+    //Main Method
     public static void main(String[] args) {
 
+
+
     }
+
+
+
+
+
 }
