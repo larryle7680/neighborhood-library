0ff5e3a6de5a03e12e634e8345daf96013493699
Larry <larryle7680@gmail.com>
2026-04-19 11:07:50 -0500

    Created Attributes for the class "Book"

---
+++ src/main/java/com/pluralsight/Book.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class Book {
+    //Attributes of the books
+    int id = 0;
+    String isbn = "";
+    boolean isCheckOut = false;
+    String checkedOutTo = "";
+
+    //Constructor
+
+
+}
--- src/main/java/com/pluralsight/NeighborhoodLibraryApp.java
+++ src/main/java/com/pluralsight/NeighborhoodLibraryApp.java
@@ -1,4 +1,8 @@
 package com.pluralsight;
 
 public class NeighborhoodLibraryApp {
+
+    public static void main(String[] args) {
+
+    }
 }
