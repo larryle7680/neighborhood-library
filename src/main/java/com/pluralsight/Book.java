5798e73b6b8079ddea60a39d4682146c25e24bd0
Larry <larryle7680@gmail.com>
2026-04-19 11:09:44 -0500

    Generated Setter/Getters for the class "Book"

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -7,11 +7,44 @@ public class Book {
     boolean isCheckOut = false;
     String checkedOutTo = "";
 
-    //Constructor
+    //Generated Constructor
     public Book(int id, String isbn, boolean isCheckOut, String checkedOutTo) {
         this.id = id;
         this.isbn = isbn;
         this.isCheckOut = isCheckOut;
+        this.checkedOutTo = checkedOutTo;
+    }
+
+    //Generated Setters and Getters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isCheckOut() {
+        return isCheckOut;
+    }
+
+    public void setCheckOut(boolean checkOut) {
+        isCheckOut = checkOut;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
         this.checkedOutTo = checkedOutTo;
     }
 }
