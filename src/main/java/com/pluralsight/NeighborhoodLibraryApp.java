f6611322f4c15b2e7475f96147aed1141be7f19f
Larry <larryle7680@gmail.com>
2026-04-19 11:33:37 -0500

    Prompt the user the options for the menu, then use a switch statement to process the users input.

--- src/main/java/com/pluralsight/NeighborhoodLibraryApp.java
+++ src/main/java/com/pluralsight/NeighborhoodLibraryApp.java
@@ -1,4 +1,5 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class NeighborhoodLibraryApp {
     //Creating global variables, so I can access them in methods
@@ -8,8 +9,11 @@ public class NeighborhoodLibraryApp {
     static boolean isCheckedOut = false;
     static String checkOutTo = "";
 
+    //Importing a scanner
+    static Scanner theScanner = new Scanner(System.in);
+
     //Creating a global Array with an inventory of 20 books
-    static Book book[] = new Book[20];
+    static Book[] book = new Book[20];
 
     //Main Method
     public static void main(String[] args) {
@@ -17,6 +21,57 @@ public class NeighborhoodLibraryApp {
 
 
     }
+
+    //checkOutMethod
+    public static String checkOut(String name){
+
+    }
+
+
+    //Creating a menu method
+    public static void storeHomeScreen(){
+        boolean running = true;
+        while(running){
+            for(int i = 0; i < book.length; i++){
+                //Prompt the user and store their answer for later use
+                System.out.println("---Press a number for your options---");
+                System.out.println();
+                System.out.println("Press 1 to show available books");
+                System.out.println("Press 2 to show checked out books");
+                System.out.println("Press 3 to exit");
+                int usersChoice = theScanner.nextInt();
+
+
+                //Using a switch statement to process users options
+                switch(usersChoice){
+                    case 1:
+                        showAvailableBooks();
+                        break;
+                    case 2:
+                        showCheckedOutBooks();
+                        break;
+                    case 3:
+                        return;
+
+
+                }
+            }
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
